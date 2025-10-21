fe8d7c1917c56a943651bc94697599d934451732
mestredainfo <murilogomes5792@gmail.com>
2025-10-21 20:13:27 -0300

    Added php sapi name

--- app/build/includes/translate.php
+++ app/build/includes/translate.php
@@ -1,11 +1,14 @@
 <?php
-$sText = json_decode(file_get_contents(dirname(__FILE__, 3) . '/langs/' . $_ENV['MIPHANT_LANG'] . '.json'), true);
+$sText = php_sapi_name() == 'cli' ? '' : json_decode(file_get_contents(dirname(__FILE__, 3) . '/langs/' . $_ENV['MIPHANT_LANG'] . '.json'), true);
 function translate($text, ...$values): string
 {
     global $sText;
 
-    $value = empty($sText[$text]) ? $text : $sText[$text];
-    $a = sprintf($value, ...$values);
-
+    if (empty($sText)) {
+        $a = sprintf($text, ...$values);
+    } else {
+        $value = empty($sText[$text]) ? $text : $sText[$text];
+        $a = sprintf($value, ...$values);
+    }
     return $a;
 }
