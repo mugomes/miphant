8cd0c72a47b656ddd9932b25641a83c924feb55d
Mu Gomes <murilogomes5792@gmail.com>
2026-05-28 19:21:05 -0300

    Added file

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+npm run dist-linux
+
+docker run --rm -ti \
+  --volume "$(pwd):/project" \
+  --volume "${HOME}/.cache/electron:/root/.cache/electron" \
+  --volume "${HOME}/.cache/electron-builder:/root/.cache/electron-builder" \
+  -w /project \
+  electronuserland/builder:wine \
+  npm run dist-win
+
+rm -rf dist/linux-unpacked/resources/server/win32/
+
+sudo chown -R bluice:bluice dist/win-unpacked/
+rm -rf dist/win-unpacked/resources/server/linux/
+
+exit 0
